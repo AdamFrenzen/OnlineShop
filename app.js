ade560eda6476812928e1f4404759ec95c0451d5
AdamFrenzen <adam.frenzen@gmail.com>
2023-09-29 20:28:11 -0600

    User login and signup

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const createSessionConfig = require('./sessions/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth')
 const authRoutes = require('./routes/auth.routes');
 const productRoutes = require('./routes/product.routes');
 const baseRoutes = require('./routes/base.routes');
@@ -24,6 +25,8 @@ const sessionConfig = createSessionConfig();
 app.use(expressSession(sessionConfig));
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
+
+app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
